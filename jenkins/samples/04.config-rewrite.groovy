a0feb54479fc9179db40949840394541c0724501
sfutagami <sbuta23dev@yahoo.co.jp>
2020-05-09 11:31:50 +0900

    [jenkins] add: 設定ファイル読み込みサンプル追加

---
+++ jenkins/samples/04.config-rewrite.groovy
@@ -0,0 +1,55 @@
+// 設定ファイルの読み込み・書き込みを行うためのサンプル
+// 予め、userContentにファイルを配置しておく必要あり
+
+def CONFIG_PATH_YML = env.JENKINS_HOME + "/userContent/configs/config.yml"
+def CONFIG_PATH_PRO = env.JENKINS_HOME + "/userContent/configs/config.properties"
+def CONFIG_PATH_NO  = env.JENKINS_HOME + "/userContent/configs/sample-version"
+
+def datas
+
+pipeline {
+    agent any
+
+    stages {
+        stage('read yaml') {
+            steps {
+                script{
+                    datas = readYaml file: "${CONFIG_PATH_YML}"
+                    println(datas)
+                    for (host in datas.root.hosts) {
+                        build(
+                            job: '00.sub',
+                            parameters: [
+                                string(name: 'name', value: host.name),
+                                string(name: 'ip', value: host.ip)
+                            ]
+                        )
+                        println(host.name)
+                        println(host.ip)
+                    }
+                }
+            }
+        }
+        // stage('read write sample-version') {
+        //     steps {
+        //          archiveArtifacts fileName
+        //     }
+        // }
+
+        // stage('read properties') {
+        //     steps {
+        //         script{
+        //             datas = readProperties file: "${CONFIG_PATH_PRO}"
+        //             println(datas)
+        //         }
+        //     }
+        // }
+
+    }
+
+    // post {
+    //     always {
+    //         cleanWs()
+    //     }
+    // }
+}
